74da8fa3989a58bc2b60ee32c64a6b164ccbe7ed
cartusiensis <170923351+Cartusiensis@users.noreply.github.com>
2024-06-05 22:10:06 +0700

    add style

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: [
-        {
-          id: 1,
-          name: "Jayapura",
-          edit: false,
-        },
-        {
-          id: 2,
-          name: "Nabire",
-          edit: false,
-        },
-        {
-          id: 3,
-          name: "Biak",
-          edit: false,
-        },
-      ]
+      list: []
     };
   }
 
